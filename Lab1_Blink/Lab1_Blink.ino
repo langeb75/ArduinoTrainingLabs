21b166835c9d0ae96651e71862167cd82ccbbb9d
Brad Lange <blange@gulllakecs.org>
2019-12-03 14:18:58 -0500

    Potentiometer Working from 0 - 1023

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -4,15 +4,24 @@
 
    AnalogReadSerial
    Reads an anolog input on pin A0, prints to the serial monitor
-
+    
 */
 
 
 
 void setup() {
-  
+  // initialize the serial communication at 9600 bits per second
+  Serial.begin(9600);
 }
 
 void loop() {
+  // read the input on analog pin 0
+  int potentiometerValue = analogRead(A0);
+
+  // print the value of our potentiometer
+  Serial.println(potentiometerValue);
+
+  //add a delay for stability
+  delay(1);
   
 }
